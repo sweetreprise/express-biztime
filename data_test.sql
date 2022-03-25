60a97f1f5c96971a21236a3a55bcf1db8aeb96c8
maria acuna <taeny90@gmail.com>
2022-03-25 17:34:00 -0400

    created invoices table for testing

--- data_test.sql
+++ data_test.sql
@@ -11,3 +11,13 @@ CREATE TABLE companies (
     name text NOT NULL UNIQUE,
     description text
 );
+
+CREATE TABLE invoices (
+    id serial PRIMARY KEY,
+    comp_code text NOT NULL REFERENCES companies ON DELETE CASCADE,
+    amt float NOT NULL,
+    paid boolean DEFAULT false NOT NULL,
+    add_date date DEFAULT CURRENT_DATE NOT NULL,
+    paid_date date,
+    CONSTRAINT invoices_amt_check CHECK ((amt > (0)::double precision))
+);
